18fd9a698135478d155c4209b9db4939cde7f180
shefeequemohammed <shefeeque85@gmail.com>
2019-01-18 23:13:56 +0400

    changes made after codacy review

--- src/test/java/com/shef/converter/ApplicationTests.java
+++ src/test/java/com/shef/converter/ApplicationTests.java
@@ -1,13 +1,34 @@
 package com.shef.converter;
 
+import org.apache.http.HttpResponse;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpUriRequest;
+import org.apache.http.impl.client.HttpClientBuilder;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.io.IOException;
+
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class ApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+	@Test
+	public void testResponse() throws ClientProtocolException, IOException {
+		/*// Given
+		HttpUriRequest request = new HttpGet( "http://localhost:8091/api/AED/INR/10");
+
+		// When
+		HttpResponse httpResponse = HttpClientBuilder.create().build().execute( request );*/
+
+	}
 
 }
 
